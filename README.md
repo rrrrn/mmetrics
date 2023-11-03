578764b38a7e063501384e874bf36e6b350fa85d
ruinie <ruinie@umich.edu>
2023-11-02 21:56:59 -0400

    complete config

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mmetrics
+
+<!-- badges: start -->
+[![R-CMD-check](https://github.com/rrrrn/mmetrics/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/rrrrn/mmetrics/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
+
---
+++ tests/testthat/test-binary_acc.R
@@ -0,0 +1,16 @@
+test_that("binary accuracy function works", {
+  # global
+  pred = c(1,0)
+  target = c(1,1)
+  expect_equal(binary_acc(pred, target), 0.5)
+
+  # global, customize threshold
+  pred = c(0.2, 0.35)
+  target = c(0, 0)
+  expect_equal(binary_acc(pred, target, 0.4), 1)
+
+  # samplewise
+  pred = matrix(c(1,0,1,0),2,2)
+  target = t(pred)
+  expect_equal(binary_acc(pred, target, multidim_average = "samplewise"), c(0.5,0.5))
+})
